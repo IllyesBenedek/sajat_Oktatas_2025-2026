47721efffad31d53c23fc5fdb137a066f87fae9c
IllyesBenedek <benedekillyes@gmail.com>
2026-03-04 09:32:31 +0100

    fajlkezeles mappa eltavolitasa a tarolobol

--- 10.k/projek/fajlkezeles/fajl-tartalmanak-beolvasasa/fajl-tartalmanak-beolvasasa-forciklus1.1-forciklus.py
+++
@@ -1,7 +0,0 @@
-# forrasfajl = open("adatbeolvasas/szamozott_sorok.txt")
-
-# forrasfajl.close()
-
-with open("szamozott_sorok.txt", "r", encoding="UTF-8") as forrasfajl:
-    for sor in forrasfajl:
-        print(sor.strip())
--- 10.k/projek/fajlkezeles/fajl-tartalmanak-beolvasasa/fajl-tartalmanak-beolvasasa1.py
+++
@@ -1,4 +0,0 @@
-import os
-
-if os.path.exists("./adatbeolvasas/szamozott_sorok.txt"):
-    forrasfajl = open("szamozott_sorok.txt")
