9dc2bf12585a28a412e18f74b99bc26b9af0787f
Illyés Benedek <benedekillyes@gmail.com>
2026-03-04 09:34:42 +0100

    Delete 10.k/projek/fajlkezeles/adatok-kiirasa/sajat_Oktatas_2025-2026/10.k directory

--- 10.k/projek/fajlkezeles/adatok-kiirasa/sajat_Oktatas_2025-2026/10.k/projek/while/5 feladat.py
+++
@@ -1,7 +0,0 @@
-szam = int(input("Adj meg egy páros számot: "))
-
-while szam % 2 != 0:
-    print("Ez egy páratlan szám volt. Próbáld újra!")
-    szam = int(input("Adj meg egy páros számot: "))
-
-print("Köszönöm! Végre egy páros szám.")
--- 10.k/projek/fajlkezeles/adatok-kiirasa/sajat_Oktatas_2025-2026/10.k/projek/while/adatbekeres.while2.py
+++
@@ -1,5 +0,0 @@
-szo = None
-while szo != "":
-    szo = input("adj meg szavakat! ha kilépnél,a szó helyett csak egy EN üss!")
-
-print("program vége")
