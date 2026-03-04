3edb2f12fbdee21b437bd16c808295a7acd1961a
IllyesBenedek <benedekillyes@gmail.com>
2026-03-04 09:22:40 +0100

    Input mappa vegre valodi mappakent

---
+++ 10.k/projek/input/1.feladat.py
@@ -0,0 +1,7 @@
+ir_szam = input("Irányítószám: ")
+varos = input("Város: ")
+kozterulet_neve = input("Közterület neve: ")
+kozterulet_jellege = input("Közterület jellege (pl.utca) : ")
+hazszam = input("Házszám")
+
+print(ir_szam, varos, kozterulet_neve, kozterulet_jellege, hazszam)
