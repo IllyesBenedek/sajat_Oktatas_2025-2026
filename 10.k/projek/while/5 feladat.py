40271233192b08b8f1e40182f0293c2b06c7d405
IllyesBenedek <benedekillyes@gmail.com>
2026-05-15 17:52:28 +0200

    while

--- 10.k/projek/while/5 feladat.py
+++ 10.k/projek/while/5 feladat.py
@@ -4,4 +4,4 @@ while szam % 2 != 0:
     print("Ez egy páratlan szám volt. Próbáld újra!")
     szam = int(input("Adj meg egy páros számot: "))
 
-print("Köszönöm! Végre egy páros szám.")
+print("Köszönöm! Végre egy páros szám!")
