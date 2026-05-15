32ab47a5a0bba2974918866f3dfe3b127c710955
IllyesBenedek <benedekillyes@gmail.com>
2026-05-15 16:08:54 +0200

    proba-python-kesz

--- sajat_Oktatas_2025-2026-main/10.k/programozas/python/proba-python/feladat.py
+++ sajat_Oktatas_2025-2026-main/10.k/programozas/python/proba-python/feladat.py
@@ -209,7 +209,8 @@ A fájl nevét második paraméterként kapja meg a függvény.
 '''
 def string_fajlba(string, fajnev):
     with open(fajnev, "w") as f:
-     f.write(string)   
+     f.write(string)
+   
 
 
 #--------------------------
