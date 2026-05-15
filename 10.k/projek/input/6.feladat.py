79070fa1f1d2db261781cad11a4621c4af64d558
IllyesBenedek <benedekillyes@gmail.com>
2026-05-15 18:50:38 +0200

    input

--- 10.k/projek/input/6.feladat.py
+++ 10.k/projek/input/6.feladat.py
@@ -3,7 +3,7 @@ import math
 r = float(input("Sugár: "))
 
 kerulet = 2 * r * math.pi
-terulet = r**r * math.pi
+terulet = r ** 2 * math.pi
 
-print("A kör kerülete: ", kerulet)
-print("A kör területe: ", terulet)
+print("A kör kerülete: ", kerulet, "cm")
+print("A kör területe: ", terulet, "cm")
