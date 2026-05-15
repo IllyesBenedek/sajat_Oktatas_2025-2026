aa560c2d04e9e3ba88de5363806d55ae8330527b
IllyesBenedek <benedekillyes@gmail.com>
2026-05-15 17:58:44 +0200

    while

--- 10.k/projek/while/adatbekeres.while.py
+++ 10.k/projek/while/adatbekeres.while.py
@@ -1,7 +1,10 @@
 szam = int(input("adj metg egy egész számot 5 és 10 között!"))
+hibak = 0
 
-# while szam < 5 or 10 < szam:
 while not 5 <= szam <= 10:
+    hibak += 1
     szam = int(input("helytelen érték! adj meg egy számot 5 és 10 között!"))
 
 print("rendben")
+if hibak > 0:
+    print(f"Összesen {hibak} alkalommal rontottad el.")
