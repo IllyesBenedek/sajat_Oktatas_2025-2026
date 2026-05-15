3be6408871529ce270c4ed70c9cd7b48dd97f784
IllyesBenedek <benedekillyes@gmail.com>
2026-05-15 19:31:02 +0200

    fajlkezeles > fajltartalmanakbeolvasasa folytatas

--- 10.k/projek/fajlkezeles/fajl-tartalmanak-beolvasasa/fajl-tartalmanak-beolvasasa1.py
+++ 10.k/projek/fajlkezeles/fajl-tartalmanak-beolvasasa/fajl-tartalmanak-beolvasasa1.py
@@ -1,4 +1,10 @@
 import os
 
-if os.path.exists("./adatbeolvasas/szamozott_sorok.txt"):
-    forrasfajl = open("szamozott_sorok.txt")
+fajl_utvonal = "szamozott_sorok.txt"
+
+if os.path.exists(fajl_utvonal):
+    with open(fajl_utvonal, "r", encoding="UTF-8") as forrasfajl:
+        print("Sikerült megnyitni a fájlt!")
+        print(forrasfajl.readline())
+else:
+    print(f"Hiba: A fájl nem található itt: {fajl_utvonal}")
