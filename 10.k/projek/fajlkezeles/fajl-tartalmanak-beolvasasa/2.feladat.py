8376d68bbb98841c9cff52a2ef25745769210e48
IllyesBenedek <benedekillyes@gmail.com>
2026-05-15 19:13:56 +0200

    fajlkezeles > fajltartalmanakbeolvasasa > 1.feladat

--- 10.k/projek/fajlkezeles/fajl-tartalmanak-beolvasasa/2.feladat.py
+++ 10.k/projek/fajlkezeles/fajl-tartalmanak-beolvasasa/2.feladat.py
@@ -2,20 +2,19 @@ with open("parduc.txt", "r", encoding="UTF-8") as versfajl:
     vers_szovege = versfajl.read()
 
     betuk_szama = 0
+    mag_szama = 0
+    maganhangzok = "aeiouáéíóöőúüűAEIOUÁÉÍÓÖŐÚÜŰ"
+
+    # Egyetlen ciklusban megszámolunk mindent
     for karakter in vers_szovege:
         if karakter.isalpha():
             betuk_szama += 1
-
-    maganhangzok = "aeiouáéíóöőúüűAEIOUÁÉÍÓÖŐÚÜŰ"
-    mag_szama = 0
-    for karakter in vers_szovege:
         if karakter in maganhangzok:
             mag_szama += 1
 
-    szavak_listája = vers_szovege.split()
-    szavak_szama = len(szavak_listája)
+    szavak_szama = len(vers_szovege.split())
 
-    print("--A vers statisztikája---")
+    print("--- A vers statisztikája ---")
     print(f"Betűk száma: {betuk_szama}")
     print(f"Magánhangzók száma: {mag_szama}")
     print(f"Szavak száma: {szavak_szama}")
